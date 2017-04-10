bcb3cad48d6e2f30c02a4e70f9450dcdbe7845a1
Jeremias Gatjens <7gatjen@gmail.com>
2017-04-10 13:45:24 -0600

    remove display: none; from step3 and pullback to step1

--- js/script.js
+++ js/script.js
@@ -11,6 +11,22 @@ $(document).ready(function() {
              equired: true,
              minlength: 5
          }
+     },
+     messages: {
+         name: "Please enter your name",
+         email: "Please enter a valid email",
+         password: {
+             required: "Please provide a password",
+             minlength: "Your password must have a 5 characters long"
+         }
+     },
+     errorClass: "invalid",
+
+     submitHandler: function() {
+         $("#form-step-1").removeClass("animate-in");
+         $("#form-step-2").addClass("animate-in");
+         $("#step1").removeClass("selected");
+         $("#step2").addClass("selected");
      }
 }
  
