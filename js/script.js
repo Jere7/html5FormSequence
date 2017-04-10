e5588860470e041067ba78fcad4089fb2ebaa9b9
Jeremias Gatjens <7gatjen@gmail.com>
2017-04-10 13:06:17 -0600

    added a js/script.js file and some jquery code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 
-
+ $("#account-creation").validate({
+     rules: {
+         name: "required",
+         email: {
+             required: true,
+             minlength: 5
+         },
+         password {
+             equired: true,
+             minlength: 5
+         }
+     }
+}
+ 
 });
