92d4eff9e8f96bf850eaa42fc80361453837d84b
Jeremias Gatjens <7gatjen@gmail.com>
2017-04-11 18:05:10 -0600

    added animate-in aniate out css

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-// $(document).ready(function() {
-
-//  $("#account-creation").validate({
-//      rules: {
-//          fullname: "required",
-//          email: {
-//              required: true,
-//              email: true 
-//          },
-//          password: {
-//              required: true,
-//              minlength:5
-//          }
-//      },
-//      messages: {
-//          fullname: "Please enter your name",
-//          email: "Please enter a valid email",
-//          password: {
-//              required: "Please provide a password",
-//              minlength: "Your password must have a 5 characters long"
-//          }
-//      },
-//      errorClass: "invalid",
-
-// 	submitHandler: function() {
-// 			$("#form-step-1").removeClass("animate-in");
-// 			$("#form-step-1").addClass("animate-out");
-			
-// 			$("#step1").removeClass("selected");
-// 			$("#step2").addClass("selected");
-
-// 			$("#form-step-2").addClass("animate-in");
-// 			$("#form-step-2").removeAttr("style");
-
-// 		}
-// 	});
 $(document).ready(function() {
 
 
